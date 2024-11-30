71ff8713c07631d0ab602bcace573b66a6f7dce5
Antoni Bertolin <tonibm1220@gmail.com>
2024-11-30 21:31:55 +0100

    Update c_the_mutex.h
    Fixed issues

--- c_the_mutex.h
+++ c_the_mutex.h
@@ -45,9 +45,12 @@ int   unlockTheMutexWithData(TheMutex* the_mutex, void** data);
 
 static void destroyGuard(TheGuard **guard_ptr) {
   if (guard_ptr&&*guard_ptr) {
-    if ((*guard_ptr)->mutex_ptr)
-    pthread_mutex_unlock((*guard_ptr)->mutex_ptr);
-    (*guard_ptr)->mutex_ptr=NULL;
+    if ((*guard_ptr)->mutex_ptr){
+      printf("Unlocking...");
+      pthread_mutex_unlock((*guard_ptr)->mutex_ptr);
+      (*guard_ptr)->mutex_ptr=NULL;
+      printf("Unlocked...");
+    }
     if ((*guard_ptr)->data)(*guard_ptr)->data=NULL;
   }
 }
@@ -63,28 +66,38 @@ static void destroyGuard(TheGuard **guard_ptr) {
 //     } while (0)
 
 #define CREATE_MUTEX_GUARD(mutex_wrap, mutex_guard_ptr)                                       \
-         TheGuard *__guard __attribute__((cleanup(destroyGuard))) = alloca(sizeof(TheGuard));                         \
-        __guard->mutex_ptr = &((mutex_wrap)->mutex_internal);               \
-        __guard->data = (mutex_wrap)->data;                                   \
-        if (pthread_mutex_lock(__guard->mutex_ptr) != 0) {                    \
+         TheGuard *mutex_guard_ptr __attribute__((cleanup(destroyGuard))) = alloca(sizeof(TheGuard));                         \
+         printf("Creating mutex guard...\n");\
+        mutex_guard_ptr->mutex_ptr = &((mutex_wrap)->mutex_internal);               \
+        mutex_guard_ptr->data = (mutex_wrap)->data;                                   \
+         printf("Copied data...\n");\
+        if (pthread_mutex_lock(mutex_guard_ptr->mutex_ptr) != 0) {                    \
             perror("Failed to lock mutex");                                 \
             exit(EXIT_FAILURE);                                             \
         }                                                                   \
-        mutex_guard_ptr=__guard; /* Return the guard pointer */                               \
+         printf("Locked...\n");\
+        // mutex_guard_ptr=__guard; /* Return the guard pointer */                               \
 
 // Sample critical section of code
 void critical_section(TheMutex* wrapper) {
-  TheGuard* guard;
   CREATE_MUTEX_GUARD(wrapper,guard);
   printf("Critical section running...\n");
   for (int i=0;i<DATA_SIZE;i++){
-    printf("Num:%i",(intptr_t)guard->data);
-    // printf("%i,", ((int*)guard->data)[i]);
+    printf("Num:%i\n",((int*)guard->data)[i]);
+  }
+  destroyGuard(&guard);
+  for (int i=0;i<DATA_SIZE;i++){
+    printf("Num:%p\n",((void*)guard->data));
+  }
+  CREATE_MUTEX_GUARD(wrapper,guard2);
+  for (int i=0;i<DATA_SIZE;i++){
+    printf("Num:%i\n",((int*)guard2->data)[i]);
   }
 }
 
 int main() {
   int* data=(int*)calloc(DATA_SIZE,sizeof(int));
+  for (int i=0;i<DATA_SIZE;i++) data[i]=INT_MAX-i;
   TheMutex wrapper;
   pthread_mutex_init(&wrapper.mutex_internal, NULL);
   wrapper.data=data;
